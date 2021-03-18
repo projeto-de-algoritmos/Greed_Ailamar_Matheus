4573c9e1d7e603e3a87f71807b5c3f0f51945c77
Matheus Amaral <mam.macacod@gmail.com>
2021-03-17 22:49:29 -0300

    added scheduling

---
+++ back-end/greedies/intervalScheduling.js
@@ -0,0 +1,35 @@
+const Task = require('../classes/task');
+const jobs = require('../data/ordenado');
+
+
+
+
+function orderByFinish() {
+    
+}
+
+
+
+function scheduling() {
+
+    let tasks = [];
+    for (const job of jobs) {
+        tasks.push(new Task({task : job}))
+    }
+    // sort(tasks);
+
+    let currenttask = [0];
+    for (let i = 0; i < tasks.length; i++) {
+        if(tasks[i]._start >= tasks[currenttask[currenttask.length - 1]]._finish) {
+            currenttask.push(i);
+        }
+    }
+
+    return currenttask;
+}
+
+module.exports = scheduling;
+
+// organizar tarefas do dia a dia
+// organizar trabalho de matérias
+// fluxo de matérias 
---
+++ back-end/index.js
@@ -0,0 +1,5 @@
+const scheduling = require('./greedies/intervalScheduling');
+
+let log = scheduling();
+console.log(log)
+return;

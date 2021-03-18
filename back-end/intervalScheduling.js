28fe25aebb707489582aa52429d96e2a9e9dffcc
Ailamar Alves <ailamar.alvesg@gmail.com>
2021-03-17 21:26:23 -0300

    Add inteval scheduling file

---
+++ back-end/intervalScheduling.js
@@ -0,0 +1,28 @@
+function sort?() {
+
+}
+
+class Task {
+    
+
+}
+
+function scheduling() {
+
+    var tasks = new task;
+
+    sort(tasks);
+
+    let currenttask = [];
+    for (let i = 1; i < tasks.length; i++) {
+        if(task.start >= task.finish) {
+            currenttask = tasks[i];
+        }
+    }
+
+    return currenttasks;
+}
+
+// organizar tarefas do dia a dia
+// organizar trabalho de matérias
+// fluxo de matérias 

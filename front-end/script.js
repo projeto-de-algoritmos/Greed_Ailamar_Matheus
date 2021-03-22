92cdc7ae3c8a683c49b6ad2e25773ad8e570afbe
Ailamar Alves <ailamar.alvesg@gmail.com>
2021-03-22 20:21:06 -0300

    Add input box

--- front-end/script.js
+++ front-end/script.js
@@ -1,20 +1,14 @@
-function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('images')
-    var data = new Date()
-    var hora = data.getHours()
-    // var hora = 22
+let submitButton;
 
-    msg.innerHTML = `Agora são ${hora} horas.`
+$(document).ready(function(){
+    submitButton = $("#submit");
 
-    if(hora >= 0 && hora < 12) {
-        img.src = 'manhas.jpg'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora < 18){
-        img.src = 'tardes.jpg'
-        document.body.style.background = '#b9846f'
-    } else {
-        img.src = 'noites.jpg'
-        document.body.style.background = '#191970'
-    }
+    submitButton.click(Adicionar);
+    
+})
+
+function adicionar() {
+    var task = document.getElementById('task')
+    var duration = document.getElementById('duration')
+    var deadline = document.getElementById('deadline')
 }
